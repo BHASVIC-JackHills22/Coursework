dc4e0bacc62e95fb5afa693eb33fe5839857582a
BHASVIC-JackHills22 <137883605+BHASVIC-JackHills22@users.noreply.github.com>
2023-10-13 14:00:44 +0100

    Movement is done

--- Tetris - Coursework/Assets/MoveBlocksScript.cs
+++ Tetris - Coursework/Assets/MoveBlocksScript.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class MoveBlocksScript : MonoBehaviour
 {
     public Rigidbody2D rigidbody2D;
-    private int frames = 0;
+    private float timeSinceMove;
+    private bool colliderC = true;
     // Start is called before the first frame update
     void Start()
     {
-        float myTime = (float)Time.time;
+
     }
 
     // Update is called once per frame
@@ -17,29 +18,48 @@ public class MoveBlocksScript : MonoBehaviour
     {
         
         moveLeft();
+        moveRight();
+        moveDown();
         autoMoveDown();
     }
 
     private void moveLeft()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && colliderC == true)
         {
             transform.position += new Vector3(-1, 0, 0);
+        }
+    }
+
+    private void moveRight()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+    }
+
+    private void moveDown()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -1, 0);
         }
     }
 
     private void autoMoveDown()
     {
         
-        if (Time.time >= 1.25)
+        if (Time.time - timeSinceMove >= 1.25)
         {
             transform.position += new Vector3(0, -1, 0);
-            frames = 0;
-            myTime = Time.time - myTime;
+            timeSinceMove = Time.time;
         }
-        else
-        {
-            
-        }
+    }
+
+    private void collisionDetection()
+    {
+        //if(Collision2D)
+        colliderC = false;
     }
 }
